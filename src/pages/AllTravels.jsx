a2e79fad37e5719e966757b6b7961867507be86f
BassaniDavid <davbassan98@gmail.com>
2025-05-16 16:45:11 +0200

    feat: stile variations

--- src/pages/AllTravels.jsx
+++ src/pages/AllTravels.jsx
@@ -4,8 +4,8 @@ import SearchBarTravel from "../components/SearchBarTravel";
 const AllTravels = () => {
     return (
         <div>
-            <div>
-                <h2 className="my-5 text-center text-light">naviga tra tutti i nostri viaggi!</h2>
+            <div className="d-flex flex-row my-5 justify-content-between mx-4">
+                <h2 className="text-center text-light">naviga tra tutti i nostri viaggi!</h2>
                 <SearchBarTravel />
             </div>
             <TravelCard />
