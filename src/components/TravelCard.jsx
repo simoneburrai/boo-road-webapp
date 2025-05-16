5b4eae049c8ed1927ddd54be8dd75a5cef0963a3
BassaniDavid <davbassan98@gmail.com>
2025-05-16 15:29:30 +0200

    feat: travel card second version

--- src/components/TravelCard.jsx
+++ src/components/TravelCard.jsx
@@ -1,17 +1,18 @@
 const TravelCard = () => {
 
     return (
-        <div className="card d-flex flex-row justify-content-around">
-            <div>immagine</div>
-            <div>
-                <h3>destinazione</h3>
-                <div className="d-flex flex-row">
-                    <h4>data inizio</h4>
-                    <h4>data fine</h4>
+        <div className="card d-flex flex-row justify-content-around mb-4">
+            <div className="w-25">immagine</div>
+            <div className="w-50">
+                <h3 className="p-2 m-4 text-center">destinazione</h3>
+                <div className="d-flex flex-row justify-content-between">
+                    <h4 className="p-2 mb-3 text-center">data inizio</h4>
+                    <h5 className="p-2 mb-3 text-center">durata</h5>
+                    <h4 className="p-2 mb-3 text-center">data fine</h4>
                 </div>
-                <div className="d-flex flex-row">
-                    <h4>costo</h4>
-                    <button>dettagli</button>
+                <div className="d-flex flex-row justify-content-between ">
+                    <h4 className=" mb-4">costo</h4>
+                    <button className="btn btn-primary mb-4">dettagli</button>
                 </div>
 
             </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import TravelCard from "../components/TravelCard";
 const Home = () => {
     return (
         <>
-            <h2>Scopri i nostri prossimi viaggi!</h2>
+            <h2 className="my-5 text-center">Scopri i nostri prossimi viaggi!</h2>
             <TravelCard />
         </>
     )
