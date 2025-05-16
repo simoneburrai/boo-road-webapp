b5f93c691e1991815346bce7eedf185f1da74e21
martinolanza03 <lanzamartino3@gmail.com>
2025-05-16 16:41:22 +0200

    fix: fixed search

--- src/pages/AllTravels.jsx
+++ src/pages/AllTravels.jsx
@@ -1,14 +1,30 @@
 import TravelCard from "../components/TravelCard";
 import SearchBarTravel from "../components/SearchBarTravel";
+import travels from "../data/travels";
+import { useState } from "react";
 
 const AllTravels = () => {
+    const [filteredTravels, setFilteredTravels] = useState(travels);
+    const [searchCity, setSearchCity] = useState("");
+
+    const handleSearch = (city) => {
+        setSearchCity(city);
+        if (city === "") {
+            setFilteredTravels(travels);
+        } else {
+            const filtered = travels.filter(travel =>
+                travel.destination.city.toLowerCase().includes(city.toLowerCase())
+            );
+            setFilteredTravels(filtered);
+        }
+    };
     return (
         <div>
             <div>
                 <h2 className="my-5 text-center text-light">naviga tra tutti i nostri viaggi!</h2>
-                <SearchBarTravel />
+                <SearchBarTravel onSearch={handleSearch} />
             </div>
-            <TravelCard />
+            <TravelCard travels={filteredTravels} />
         </div>
     )
 }
