c055f24770bd7ab0b4ec20bfbd0c35706e655f9e
martinolanza03 <lanzamartino3@gmail.com>
2025-05-16 16:32:40 +0200

    feat: add text in the travel card

--- src/components/TravelCard.jsx
+++ src/components/TravelCard.jsx
@@ -6,14 +6,14 @@ const TravelCard = () => {
         return <div className="card d-flex flex-row justify-content-around mb-4" key={index}>
             <div className="w-25">{travel.image}</div>
             <div className="w-50">
-                <h3 className="p-2 m-4 text-center">{travel.destination.city}</h3>
+                <h3 className="p-2 m-4 text-center">Destinazione: {travel.destination.city}</h3>
                 <div className="d-flex flex-row justify-content-between">
-                    <h4 className="p-2 mb-3 text-center">{travel.start}</h4>
-                    <h5 className="p-2 mb-3 text-center">{travel.duration}</h5>
-                    <h4 className="p-2 mb-3 text-center">{travel.end}</h4>
+                    <h4 className="p-2 mb-3 text-center">Inizio: {travel.start}</h4>
+                    <h5 className="p-2 mb-3 text-center">{travel.duration} Giorni</h5>
+                    <h4 className="p-2 mb-3 text-center">Fine: {travel.end}</h4>
                 </div>
                 <div className="d-flex flex-row justify-content-between ">
-                    <h4 className=" mb-4">{travel.price}</h4>
+                    <h4 className=" mb-4">{travel.price}€</h4>
                     <button className="btn btn-primary mb-4">dettagli</button>
                 </div>
             </div>
