aa0b440738d977f89ba655cff0660028034f148f
BassaniDavid <davbassan98@gmail.com>
2025-05-16 15:52:56 +0200

    feat: import travel details card in single travel

--- src/pages/SingleTravel.jsx
+++ src/pages/SingleTravel.jsx
@@ -1,8 +1,10 @@
 import OperatorCard from "../components/OperatorCard";
+import TravelDetailCard from "../components/travelDetailCard";
 
 const SingleTravel = () => {
     return (
         <div>
+            <TravelDetailCard />
             <OperatorCard />
         </div>
     )
