61b036a0f91c6e70c3884fffccda4819de6034eb
BassaniDavid <davbassan98@gmail.com>
2025-05-16 16:22:06 +0200

    feat: small changes

--- src/pages/AllTravels.jsx
+++ src/pages/AllTravels.jsx
@@ -4,9 +4,10 @@ import SearchBarTravel from "../components/SearchBarTravel";
 const AllTravels = () => {
     return (
         <div>
-            <h2 className="my-5 text-center">naviga tra tutti i nostri viaggi!</h2>
-            <SearchBarTravel />
-            <h2 className="my-5 text-center text-light">naviga tra tutti i nostri viaggi!</h2>
+            <div>
+                <h2 className="my-5 text-center">naviga tra tutti i nostri viaggi!</h2>
+                <SearchBarTravel />
+            </div>
             <TravelCard />
         </div>
     )
