2d18632c96a5cf70c99374e143771805d8ebc850
martinolanza03 <lanzamartino3@gmail.com>
2025-05-16 15:54:42 +0200

    feat: import search bar in AllTravels.jsx

--- src/pages/AllTravels.jsx
+++ src/pages/AllTravels.jsx
@@ -1,9 +1,11 @@
 import TravelCard from "../components/TravelCard";
+import SearchBarTravel from "../components/SearchBarTravel";
 
 const AllTravels = () => {
     return (
         <div>
             <h2 className="my-5 text-center">naviga tra tutti i nostri viaggi!</h2>
+            <SearchBarTravel />
             <TravelCard />
         </div>
     )
