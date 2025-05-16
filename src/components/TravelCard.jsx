53a7ce82e6e5c5150735876778be21b20ba1a0db
BassaniDavid <davbassan98@gmail.com>
2025-05-16 15:18:39 +0200

    feat: travel card first version

--- src/components/TravelCard.jsx
+++ src/components/TravelCard.jsx
@@ -1,7 +1,21 @@
 const TravelCard = () => {
 
     return (
-        <div></div>
+        <div className="card d-flex flex-row justify-content-around">
+            <div>immagine</div>
+            <div>
+                <h3>destinazione</h3>
+                <div className="d-flex flex-row">
+                    <h4>data inizio</h4>
+                    <h4>data fine</h4>
+                </div>
+                <div className="d-flex flex-row">
+                    <h4>costo</h4>
+                    <button>dettagli</button>
+                </div>
+
+            </div>
+        </div>
     )
 }
 
