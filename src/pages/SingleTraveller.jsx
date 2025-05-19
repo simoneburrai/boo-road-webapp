2165c0112bf1cd8f195e554de03607475e812a21
martinolanza03 <lanzamartino3@gmail.com>
2025-05-19 09:58:32 +0200

    feat: created button to quickly contact the traveler

--- src/pages/SingleTraveller.jsx
+++ src/pages/SingleTraveller.jsx
@@ -1,36 +1,37 @@
-import { useNavigate, useParams} from "react-router-dom";
-import {useTravels} from "../contexts/TravelContext"
+import { useNavigate, useParams } from "react-router-dom";
+import { useTravels } from "../contexts/TravelContext"
 import { useEffect } from "react";
 
-const SingleTraveller = ()=> {
+const SingleTraveller = () => {
     const navigate = useNavigate();
-    const {travellers} = useTravels();
-    let {id, travelId} = useParams();
+    const { travellers } = useTravels();
+    let { id, travelId } = useParams();
     id = parseInt(id);
     travelId = parseInt(travelId)
     console.log(travelId, id);
     const currentTravellers = travellers.filter(traveller => traveller.travelId === travelId)
     const currentTraveller = currentTravellers.find(traveller => traveller.id === id);
 
-   useEffect(() => {
-    if (travellers.length > 0 && (!currentTraveller || currentTravellers.length === 0)) {
-        navigate("/notfound");
-    }
-}, [travellers, currentTraveller, currentTravellers, navigate]);
+    useEffect(() => {
+        if (travellers.length > 0 && (!currentTraveller || currentTravellers.length === 0)) {
+            navigate("/notfound");
+        }
+    }, [travellers, currentTraveller, currentTravellers, navigate]);
 
 
-    if(currentTraveller && currentTravellers.length > 0) {
-          return <div>
-        <div>{currentTraveller.name}</div>
-        <div>{currentTraveller.surname}</div>
-        <div>{currentTraveller.number}</div>
-        <div>{currentTraveller.email}</div>
-        <div>{currentTraveller.nationality}</div>
-        <div>{currentTraveller.idCode}</div>
-        <div>{currentTraveller.emergencyNumber}</div>
-    </div>
+    if (currentTraveller && currentTravellers.length > 0) {
+        return <div>
+            <div>{currentTraveller.name}</div>
+            <div>{currentTraveller.surname}</div>
+            <div>{currentTraveller.number}</div>
+            <div>{currentTraveller.email}</div>
+            <div>{currentTraveller.nationality}</div>
+            <div>{currentTraveller.idCode}</div>
+            <div>{currentTraveller.emergencyNumber}</div>
+            <button className="btn btn-primary mb-4">Contatta</button>
+        </div>
     }
-  
+
 }
 
 export default SingleTraveller;
