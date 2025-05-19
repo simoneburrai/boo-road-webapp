260443d518236ebb66949cdbca77b22096935879
BassaniDavid <davbassan98@gmail.com>
2025-05-19 11:00:50 +0200

    feat: changed header font

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,8 @@ import { NavLink } from "react-router-dom";
 const Header = () => {
     return <header>
         <nav className="container d-flex justify-content-between  px-3 py-4">
-            <NavLink to="/" className="text-decoration-none text-uppercase fs-3 mx-4">boroad</NavLink>
-            <NavLink to="/travels" className="text-decoration-none text-uppercase fs-3 mx-4">Travels</NavLink>
+            <NavLink to="/" className="text-decoration-none text-uppercase fs-3 mx-4 boroad"> <span>bo</span>road</NavLink>
+            <NavLink to="/travels" className="text-decoration-none text-uppercase fs-3 mx-4 boroad">Travels</NavLink>
         </nav>
         <input type="checkbox" name="toggleMode" id="toggleMode" /><label htmlFor="toggleMode"><i className="fas fa-sun"></i></label>
     </header>
