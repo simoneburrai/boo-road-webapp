130b76094b2278ebf0b3aefad89a1d1083a0f314
BassaniDavid <davbassan98@gmail.com>
2025-05-19 16:07:52 +0200

    feat: traveldetail card responsive completed

--- src/components/travels/TravelDetailCard.jsx
+++ src/components/travels/TravelDetailCard.jsx
@@ -11,17 +11,17 @@ const TravelDetailCard = ({ travel }) => {
         <div className="card">
             <div className=" d-flex flex-column flex-md-row justify-content-around mb-2 mb-lg-5">
                 <div className="detail-img image">
-                    <img className="w-100 p-2" src={travel.image} alt={travel.destination.city} />
+                    <img className="w-100 p-2 " src={travel.image} alt={travel.destination.city} />
                 </div>
-                <div className=" p-1 p-lg-2 px-2 px-lg-5">
+                <div className="p-3 px-2 px-lg-5 detailtext">
                     <h3 className="m-2 m-lg-4 text-center fst-italic fs-1">{travel.destination.city}</h3>
                     <div className="d-flex flex-row justify-content-between align-items-center mb-2 mb-lg-5">
-                        <h4 className="mb-3">{formattedStart}</h4>
-                        <h5 className="p-2 mb-3 fst-italic">{travel.duration} giorni</h5>
-                        <h4 className="p-2 mb-3">{formattedEnd}</h4>
+                        <h4 className="mb-3 traveltext">{formattedStart}</h4>
+                        <h5 className="p-2 mb-3 fst-italic traveltext">{travel.duration} giorni</h5>
+                        <h4 className="p-2 mb-3 traveltext">{formattedEnd}</h4>
                     </div>
-                    <div className="d-flex flex-row justify-content-around">
-                        <h4 className=" mb-4">Costo: {travel.price} €</h4>
+                    <div className="d-flex flex-row justify-content-around align-items-center">
+                        <h4 className=" mb-4 mx-2 traveltext flex-shrink-0">Costo: {travel.price} €</h4>
                         <Link to={`/travels/${travel.id}/travellers`}
                             className="btn btn-primary mb-4"
                         >Vedi tutti i viaggiatori</Link>
