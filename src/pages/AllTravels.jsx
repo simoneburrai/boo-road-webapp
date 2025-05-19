895a6bfdec6c6348039a0226f4914b3e6b9deecb
martinolanza03 <lanzamartino3@gmail.com>
2025-05-19 12:14:16 +0200

    feat: add component newTravel

--- src/pages/AllTravels.jsx
+++ src/pages/AllTravels.jsx
@@ -1,11 +1,10 @@
 import TravelCard from "../components/travels/TravelCard";
 import SearchBarTravel from "../components/SearchBarTravel";
-import {useTravels} from "../contexts/TravelContext"
+import { useTravels } from "../contexts/TravelContext"
 import { useState } from "react";
 
 const AllTravels = () => {
-    const {travels} = useTravels()
-    const [filteredTravels, setFilteredTravels] = useState(travels);
+    const { travels, filteredTravels, setFilteredTravels } = useTravels()
     const [searchCity, setSearchCity] = useState("");
 
     const handleSearch = (city) => {
@@ -19,7 +18,7 @@ const AllTravels = () => {
             setFilteredTravels(filtered);
         }
     };
-    
+
     return (
         <div>
             <div className="d-flex flex-row my-5 justify-content-between mx-3">
