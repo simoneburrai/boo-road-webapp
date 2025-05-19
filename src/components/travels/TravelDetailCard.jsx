96ac196cf13dcdbba5673db6e2ae07eba37e9338
giorgiameffe <giorgiameffe03@gmail.com>
2025-05-19 10:28:56 +0200

    feat: add some style to detail card

--- src/components/travels/TravelDetailCard.jsx
+++ src/components/travels/TravelDetailCard.jsx
@@ -2,42 +2,52 @@ import { Link } from "react-router-dom";
 import { format } from "date-fns";
 import { it } from "date-fns/locale";
 
-const TravelDetailCard = ({travel}) => {
+const TravelDetailCard = ({ travel }) => {
     const flight = travel.flight;
     const formattedStart = format(new Date(travel.start), "d MMMM yyyy", { locale: it });
     const formattedEnd = format(new Date(travel.end), "d MMMM yyyy", { locale: it });
 
     return (
-        <div className="card">
+        <div className="card mt-5">
             <div className=" d-flex flex-row justify-content-around mb-4">
-                <div className="w-25"><img src={travel.image} alt={travel.destination.city} /></div>
-                <div className="w-50">
-                    <h3 className="p-2 m-4 text-center">{travel.destination.city}</h3>
-                    <div className="d-flex flex-row justify-content-between">
-                        <h4 className="p-2 mb-3 text-center">{formattedStart}</h4>
-                        <h5 className="p-2 mb-3 text-center">{travel.duration} giorni</h5>
-                        <h4 className="p-2 mb-3 text-center">{formattedEnd}</h4>
+                <div className="detail-img w-50">
+                    <img className="w-100" src={travel.image} alt={travel.destination.city} />
+                </div>
+                <div className="w-100 p-4">
+                    <h3 className="p-2 m-4 text-center fst-italic text-danger fs-1">{travel.destination.city}</h3>
+                    <div className="d-flex flex-row justify-content-between align-items-center">
+                        <h4 className="mb-3">{formattedStart}</h4>
+                        <h5 className="p-2 mb-3 fst-italic">{travel.duration} giorni</h5>
+                        <h4 className="p-2 mb-3">{formattedEnd}</h4>
                     </div>
-                    <div className="d-flex flex-row justify-content-between ">
-                        <h4 className=" mb-4">costo</h4>
+                    <div className="d-flex flex-row justify-content-around">
+                        <h4 className=" mb-4">Costo: {travel.price} €</h4>
                         <Link to={`/travels/${travel.id}/travellers`}
-                        className="btn btn-primary mb-4"
+                            className="btn btn-primary mb-4"
                         >Vedi tutti i viaggiatori</Link>
                     </div>
                 </div>
             </div>
-            <div>
-                <p>{travel.description}</p>
-                {flight && <div>
-                    <h5>dettagli volo</h5>
-                    <div>Compagnia: {flight.airline}</div>
-                    <div>Numero Volo: {flight.flightNumber}</div>
-                    <div>Partenza Aereoporto:  {flight.departureAirport}</div>
-                    <div>Arrivo Aereoporto: {flight.arrivalAirport}</div>
+            <div className="p-2">
+                <p className="fst-italic fs-3">{travel.description}</p>
+                {flight && <div className="p-2">
+                    <h5 className="text-danger fs-3 mb-4">Dettagli volo</h5>
+                    <ul className="list-unstyled fs-5">
+                        <li>
+                            Compagnia: <span className="fw-bold">{flight.airline}</span>
+                        </li>
+                        <li>
+                            Numero Volo: <span className="fw-bold">{flight.flightNumber}</span>
+                        </li>
+                        <li>
+                            Partenza Aeroporto: <span className="fw-bold">{flight.departureAirport}</span>
+                        </li>
+                        <li>
+                            Arrivo Aeroporto: <span className="fw-bold">{flight.arrivalAirport}</span>
+                        </li>
+                    </ul>
                 </div>}
-                
             </div>
-
         </div>
     )
 }
