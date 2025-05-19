637c803fedb201fad9e1c3534a036612b51570d4
BassaniDavid <davbassan98@gmail.com>
2025-05-19 16:25:36 +0200

    feat: home css small changes

--- src/components/travels/TravelCard.jsx
+++ src/components/travels/TravelCard.jsx
@@ -11,18 +11,18 @@ const TravelCard = ({ travels }) => {
                     const formattedEnd = format(new Date(travel.end), "d MMMM yyyy", { locale: it });
 
                     return (
-                        <div className="card travelcard d-flex flex-row justify-content-around mb-3 mb-md-4 p-md-3 p-2" key={travel.id}>
-                            <img className="rounded imagecard" src={travel.image} alt="image" />
-                            <div className="w-50">
+                        <div className="card travelcard d-flex flex-column flex-sm-row justify-content-around mb-3 mb-md-4 p-md-3 p-2" key={travel.id}>
+                            <img className="rounded imagecard m-2 m-sm-1" src={travel.image} alt="image" />
+                            <div className="p-2">
                                 <h3 className="p-1 p-md-2 m-md-3 m-2 m-md-4 text-center">Destinazione: {travel.destination.city}</h3>
                                 <div className="d-flex flex-row justify-content-between  mb-4 durata px-3">
                                     <h4 className="p-1 p-md-2 text-center">Inizio: {formattedStart}</h4>
                                     <h5 className="p-1 p-md-2 text-center">{travel.duration} Giorni</h5>
                                     <h4 className="p-1 p-md-2 text-center">Fine: {formattedEnd}</h4>
                                 </div>
-                                <div className="d-flex flex-row flex-wrap justify-content-between">
+                                <div className="d-flex flex-row flex-wrap justify-content-between align-items-center">
                                     <h6 className={`mb-4 p-1 p-md-2 px-3 price ${travel.discountPrice && "line-through"}`}>{travel.price}€</h6>
-                                    {travel.discountPrice && <h6 className="mb-4 p-md-2 px-3 discount">{travel.discountPrice}€</h6>}
+                                    {travel.discountPrice && <h6 className="mb-4 p-1 p-md-2 px-3 discount">{travel.discountPrice}€</h6>}
                                     <Link to={`/travels/${travel.id}`} className="btn btn-primary mb-4 p-md-2">Dettagli</Link>
                                 </div>
                             </div>
