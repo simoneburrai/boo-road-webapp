427c5943227c06eb09d3d41f511f976a9bee1ae1
martinolanza03 <lanzamartino3@gmail.com>
2025-05-19 15:03:25 +0200

    fix: fix className

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
         <nav className="container d-flex justify-content-between  px-3 py-4">
             <NavLink to="/" className="text-decoration-none text-uppercase mx-4 boroad"> <span>boo</span>road</NavLink>
             <div>
-                <NavLink to="/travels" className="text-decoration-none text-uppercasemx-4 boroad">Travels</NavLink>
+                <NavLink to="/travels" className="text-decoration-none text-uppercase mx-4 boroad">Travels</NavLink>
                 <NavLink to="/newtravel" className="text-decoration-none text-uppercase mx-4 boroad">New Travel</NavLink>
             </div>
             <input type="checkbox" name="toggleMode" id="toggleMode" /><label className="darkmode" htmlFor="toggleMode"><i className="fas fa-sun"></i></label>
