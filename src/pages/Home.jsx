3c1d06e948e01894add371bce7d815a10f89d24a
BassaniDavid <davbassan98@gmail.com>
2025-05-19 10:38:57 +0200

    feat: added button to switch between light and dark mode

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,17 @@
 import TravelCard from "../components/travels/TravelCard";
 import filteredSortedTravels from "../utils/filteredDateTravels";
-import {useTravels} from "../contexts/TravelContext"
+import { useTravels } from "../contexts/TravelContext"
 
 
 const Home = () => {
-    const {travels} = useTravels()
+    const { travels } = useTravels()
     const nextMonths = 2;
     const filteredTravelsforDate = filteredSortedTravels(travels, nextMonths)
 
 
     return (
         <>
-            <h2 className="my-5 text-center text-light">Scopri i nostri prossimi viaggi!</h2>
+            <h2 className="my-5 text-center">Scopri i nostri prossimi viaggi!</h2>
             <TravelCard travels={filteredTravelsforDate} />
         </>
     )
