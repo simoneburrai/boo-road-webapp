e2d1765ecf87749089b3dec69f9325479ef06edd
BassaniDavid <davbassan98@gmail.com>
2025-05-19 12:20:36 +0200

    feat: css corrections

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
             <NavLink to="/" className="text-decoration-none text-uppercase fs-3 mx-4 boroad"> <span>bo</span>road</NavLink>
             <NavLink to="/travels" className="text-decoration-none text-uppercase fs-3 mx-4 boroad">Travels</NavLink>
         </nav>
-        <input type="checkbox" name="toggleMode" id="toggleMode" /><label htmlFor="toggleMode"><i className="fas fa-sun"></i></label>
+        <input type="checkbox" name="toggleMode" id="toggleMode" /><label className="darkmode" htmlFor="toggleMode"><i className="fas fa-sun"></i></label>
     </header>
 }
 
