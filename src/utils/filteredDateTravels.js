bc3f637cb5292e1b7344bf6dfe47bceb03e560ee
simoneburrai <burrai.s98@gmail.com>
2025-05-19 10:24:57 +0200

    feat: filteredDate changed, reading only 2 months

--- src/utils/filteredDateTravels.js
+++ src/utils/filteredDateTravels.js
@@ -1,18 +1,19 @@
-import { parseISO, differenceInMonths, compareAsc } from "date-fns";
+import { parseISO, differenceInMonths, isAfter, isEqual, compareAsc } from 'date-fns';
 
+const filteredSortedTravels = (travels, monthsFilter) => {
+  const today = new Date();
 
-const filteredSortedTravels = (travels, monthsFilter)=>{
-
-    return travels //riordino per data di inizio, filtro solo quelli nei prossimi 2 mesi
-  .map(travel => ({
-    ...travel,
-    startDate: parseISO(travel.start) // parsing ISO date
-  }))
-  .filter(travel => differenceInMonths(travel.startDate, new Date()) <= monthsFilter)
-  .sort((a, b) => compareAsc(a.startDate, b.startDate))
-  .map(({ startDate, ...rest }) => rest);
-}
-
-
+  return travels
+    .map(travel => ({
+      ...travel,
+      startDate: parseISO(travel.start)
+    }))
+    .filter(travel =>
+      (isAfter(travel.startDate, today) || isEqual(travel.startDate, today)) &&
+      differenceInMonths(travel.startDate, today) <= monthsFilter
+    )
+    .sort((a, b) => compareAsc(a.startDate, b.startDate))
+    .map(({ startDate, ...rest }) => rest);
+};
 
 export default filteredSortedTravels;
