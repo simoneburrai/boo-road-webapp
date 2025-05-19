4f9e6de1d013698c5b702f7e228d9529aebb2715
simoneburrai <burrai.s98@gmail.com>
2025-05-19 11:40:50 +0200

    feat: modified colors prices

--- src/components/travels/TravelCard.jsx
+++ src/components/travels/TravelCard.jsx
@@ -21,8 +21,8 @@ const TravelCard = ({ travels }) => {
                                     <h4 className="p-2 mb-3 text-center">Fine: {formattedEnd}</h4>
                                 </div>
                                 <div className="d-flex flex-row justify-content-between">
-                                    <h4 className="mb-4">{travel.price}€</h4>
-                                    {travel.discountPrice && <h4 className="mb-4">{travel.discountPrice}€</h4>}
+                                    <h4 className={`mb-4 btn btn-success ${travel.discountPrice && "line-through"}`}>{travel.price}€</h4>
+                                    {travel.discountPrice && <h4 className="mb-4 btn btn-danger">{travel.discountPrice}€</h4>}
                                     <Link to={`/travels/${travel.id}`} className="btn btn-primary mb-4">dettagli</Link>
                                 </div>
                             </div>
