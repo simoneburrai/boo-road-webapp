bb8bcbe6005a32190ea809c6be86498c85a3adc7
martinolanza03 <lanzamartino3@gmail.com>
2025-05-20 09:27:28 +0200

    fix: change class with className

--- src/components/travels/OperatorCard.jsx
+++ src/components/travels/OperatorCard.jsx
@@ -4,10 +4,10 @@ const OperatorCard = ({ operator }) => {
 
         <div className="card mb-3 mt-3 p-3">
             <div className="w-md-50">
-                <div className=""><strong><i class="fa-solid fa-user"></i> Nome:</strong> {name}</div>
-                <div><strong><i class="fa-solid fa-user"></i> Cognome:</strong> {surname}</div>
-                <div><strong><i class="fa-solid fa-phone"></i> Numero di telefono:</strong> {number} </div>
-                <div><strong><i class="fa-solid fa-envelope"></i> Email: </strong>{email}</div>
+                <div className=""><strong><i className="fa-solid fa-user"></i> Nome:</strong> {name}</div>
+                <div><strong><i className="fa-solid fa-user"></i> Cognome:</strong> {surname}</div>
+                <div><strong><i className="fa-solid fa-phone"></i> Numero di telefono:</strong> {number} </div>
+                <div><strong><i className="fa-solid fa-envelope"></i> Email: </strong>{email}</div>
             </div>
         </div>
     )
